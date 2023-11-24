3ef5e6a4defda300037a8f512f319eb8d96a3166
ankit <av6007616@gmail.com>
2023-11-24 23:28:26 +0530

    new library installed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './pages/login/login';
+import Signup from './pages/signup/signup';
+
+function App() {
+  return (
+    <div className="App">
+      <Signup/>
+    </div>
+  );
+}
+
+export default App;
